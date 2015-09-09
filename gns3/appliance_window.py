f192e805f82ca1c183610c055ab4349a7d4e968d
Julien Duponchelle <julien@duponchelle.info>
2015-09-09 17:42:23 +0200

    Start the install button

--- gns3/appliance_window.py
+++ gns3/appliance_window.py
@@ -24,6 +24,7 @@ from .ui.appliance_window_ui import Ui_ApplianceWindow
 from .image_manager import ImageManager
 from .registry.appliance import Appliance
 from .registry.registry import Registry
+from .registry.config import Config
 
 import logging
 log = logging.getLogger(__name__)
@@ -44,11 +45,23 @@ class ApplianceWindow(QtWidgets.QWidget, Ui_ApplianceWindow):
         self.setupUi(self)
         self.setWindowTitle(path)
 
+        self._path = path
+
         # Call linkClickedSlot() for all non local links
         self.uiWebView.page().setLinkDelegationPolicy(QtWebKitWidgets.QWebPage.DelegateExternalLinks)
         self.uiWebView.linkClicked.connect(self._linkClickedSlot)
 
+        # Expose JavaScript objects
+        self.uiWebView.page().mainFrame().javaScriptWindowObjectCleared.connect(self.javaScriptWindowObject)
 
+        # Enable the inspector on right click
+        self.uiWebView.settings().setAttribute(QtWebKit.QWebSettings.DeveloperExtrasEnabled, True)
+
+        self._refresh()
+
+        self.show()
+
+    def _refresh(self):
         renderer = jinja2.Environment(loader=jinja2.FileSystemLoader(get_resource('templates')))
         renderer.filters['nl2br'] = lambda s: s.replace('\n', '<br />')
         renderer.filters['human_filesize'] = human_filesize
@@ -57,13 +70,53 @@ class ApplianceWindow(QtWidgets.QWidget, Ui_ApplianceWindow):
         registry = Registry(ImageManager.instance().getDirectory())
 
         #TODO: Catch error
-        appliance = Appliance(registry, path)
+        self._appliance = Appliance(registry, self._path)
 
-        self.uiWebView.setHtml(template.render(appliance=appliance))
-        self.show()
+        self.uiWebView.setHtml(template.render(appliance=self._appliance))
+
+    def javaScriptWindowObject(self):
+        frame = self.uiWebView.page().mainFrame()
+        frame.addToJavaScriptWindowObject('gns3', self)
 
     def _linkClickedSlot(self, url):
         """
         Open in a new browser other url
         """
         QtGui.QDesktopServices.openUrl(url)
+
+    #
+    # Public Javascript methods
+    #
+    @QtCore.pyqtSlot(str, result=bool)
+    def install(self, version):
+        """
+        Install an appliance based on appliance version
+
+        :param version: Version to install
+        """
+
+        config = Config()
+        appliance_configuration = self._appliance.search_images_for_version(version)
+
+        if config.servers == ["local"]:
+            server = "local"
+        else:
+            server_types = {}
+            for server in Config().servers:
+                if server == "local":
+                    server_types["Local server"] = server
+                elif server == "vm":
+                    server_types["GNS3 VM"] = server
+                else:
+                    server_types[server] = server
+            selection, ok = QtWidgets.QInputDialog.getItem(self.parent(), "GNS3 server", "Please select a GNS3 server:", list(server_types.keys()), 0, False)
+            if ok:
+                server = server_types[selection]
+            else:
+                return False
+
+        # if config.add_appliance(appliance_configuration, server):
+        #     self.success.emit("{} installed".format(appliance["name"]))
+        config.save()
+        return True
+
